29bd2c723159318d7e2d677c65ef26836ac3998e
89rajkumaryadav <89rajkumaryadav@gmail.com>
2026-09-04 20:32:44 +0530

    Initial Commit

---
+++ MapMyPet/MapMyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapMyPetApp.swift
+//  MapMyPet
+//
+//  Created by Rajkumar Yadav on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MapMyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
