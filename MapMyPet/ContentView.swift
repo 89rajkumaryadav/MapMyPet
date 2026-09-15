bcf686549e2e9e6f5f85681543a90516f5659c84
89rajkumaryadav <89rajkumaryadav@gmail.com>
2026-09-15 21:41:01 +0530

    TabView added

--- MapMyPet/ContentView.swift
+++ MapMyPet/ContentView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-          }
-        .padding()
+        TabView {
+            AnimalsNearYouView()
+                .tabItem {
+                    Label("Near you", systemImage: "location")
+                }
+            
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+        }
     }
 }
 
